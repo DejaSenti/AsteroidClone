f65bcb4142df5aa2a3f2f633cf95912cf8beb269
tomlyden <tom.lyden.zzz@gmail.com>
2020-12-22 10:26:28 +0200

    Organization Added object pool Added abstract space object class

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectPool<T> where T : SpaceObject
+{
+    public int ActiveCount { get => CountActiveObjects(); }
+
+    private List<T> objectPool;
+
+    public ObjectPool()
+    {
+        objectPool = new List<T>();
+    }
+
+    public void Initialize(int poolSize)
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            var gameObject = Resources.Load<GameObject>("Assets/Prefabs/PoolObjects/" + typeof(T));
+            Object.Instantiate(gameObject);
+            gameObject.SetActive(false);
+
+            T spaceObject = gameObject.GetComponent<T>();
+            objectPool.Add(spaceObject);
+        }
+    }
+
+    public T Spawn()
+    {
+        foreach(T spaceObject in objectPool)
+        {
+            if (spaceObject.enabled)
+                return spaceObject;
+        }
+
+        return null;
+    }
+
+    private int CountActiveObjects()
+    {
+        int activeObjects = 0;
+
+        foreach(T spaceObject in objectPool)
+        {
+            if (spaceObject.enabled)
+                activeObjects++;
+        }
+
+        return activeObjects;
+    }
+}
--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using ExtensionMethods;
 
-public class PlayerShip : MonoBehaviour
+public class PlayerShip : SpaceObject
 {
+    public string ObjectTag { get => gameObject.tag; }
     public float Acceleration;
     public float AngularAcceleration;
-
-    public Rigidbody2D RB;
-    public Collider2D Collider;
 
     public Vector2 Direction { get => this.RotationToVector2(transform.rotation.eulerAngles.z); }
 }
