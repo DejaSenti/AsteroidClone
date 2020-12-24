60d8bc18c58ae12f2d9bcd3aa582dcae72cc3529
tomlyden <tom.lyden.zzz@gmail.com>
2020-12-24 10:49:34 +0200

    Sorted out camera and game boundary Started Ghost Project

---
+++ Assets/SpaceBoundary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SpaceBoundary : MonoBehaviour
+{
+    public RectTransform RectTransform;
+
+    public float Width;
+    public float Height;
+
+    private void Awake()
+    {
+        Width = RectTransform.rect.width * RectTransform.lossyScale.z;
+        Height = RectTransform.rect.height * RectTransform.lossyScale.z;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Vector3 origin = RectTransform.position;
+
+        Gizmos.DrawLine(origin + new Vector3(-Width, -Height, 0) / 2, origin + new Vector3(-Width, Height, 0) / 2);
+        Gizmos.DrawLine(origin + new Vector3(-Width, Height, 0) / 2, origin + new Vector3(Width, Height, 0) / 2);
+        Gizmos.DrawLine(origin + new Vector3(Width, Height, 0) / 2, origin + new Vector3(Width, -Height, 0) / 2);
+        Gizmos.DrawLine(origin + new Vector3(Width, -Height, 0) / 2, origin + new Vector3(-Width, -Height, 0) / 2);
+    }
+}
