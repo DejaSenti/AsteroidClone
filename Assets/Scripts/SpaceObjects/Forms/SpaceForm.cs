27bc5b15abff600647abe25ad74e00f81e37ca81
tomlyden <tom.lyden.zzz@gmail.com>
2021-01-08 21:39:53 +0200

    Made player ship sprite larger (still not enough) More particle system attempts

--- Assets/Scripts/SpaceObjects/Forms/SpaceForm.cs
+++ Assets/Scripts/SpaceObjects/Forms/SpaceForm.cs
@@ -2,20 +2,16 @@
 
 public abstract class SpaceForm : MonoBehaviour
 {
-    private const int NUM_EMITTED_PARTICLES = 25;
-
     public Collider2D Collider;
     public SpaceEntity Entity;
 
     public ParticleSystem ParticleSystem;
-    private ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
 
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
         if (Entity.isActiveAndEnabled)
         {
-            emitParams.position = transform.position;
-            ParticleSystem.Emit(emitParams, NUM_EMITTED_PARTICLES);
+            ParticleSystem.transform.position = transform.position;
             ParticleSystem.Play();
 
             Entity.OnCollision(collision);
