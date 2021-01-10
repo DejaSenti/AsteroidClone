59e0d6a89ebc4ac424694dc5361d1d7c345f8d37
tomlyden <tom.lyden.zzz@gmail.com>
2021-01-10 11:48:47 +0200

    A few bug fixes and balancing

--- Assets/Scripts/PlayerInteraction/AnnouncingService.cs
+++ Assets/Scripts/PlayerInteraction/AnnouncingService.cs
@@ -149,6 +149,7 @@ public class AnnouncingService : MonoBehaviour
     {
         LevelManager.EndLevelEvent.RemoveListener(OnEndLevel);
 
+        Clear();
         announcementQueue.Clear();
         displayTimes.Clear();
 
